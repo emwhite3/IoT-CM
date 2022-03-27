3677408ef6f16929b58719ebb8f27be87600bcf8
emwhite3 <emwhite3@miners.utep.edu>
2022-03-27 11:33:04 -0600

    started to replace attack agent with movement agent. appropriate attributes have been added

--- iot-ibl.py
+++ iot-ibl.py
@@ -28,36 +28,32 @@ def reset_agent(agent, noise=NOISE, temperature=TEMPERATURE, decay=DECAY):
 # there we can also add similarity. Once that is added we can add the validity agent
 # this agent will take in commands from labeled command providers
 # some trusted and some untrusted
-def populate_agent(select, attack):
-    select.populate(110, 0)
-    select.populate(110, 1)
-    attack.populate(-30, {"attack": False, "warning": 0})
-    attack.populate(110, {"attack": True, "warning": 0})
-    attack.populate(-55, {"attack": True, "warning": 0})
-    select.instances()
-    attack.instances()
+def populate_agent(movement):
+    movement.populate(-30, {"x": 0, "y": 0, "safe": False, "success": False, "trust": False})
+    movement.populate(110, {"attack": True, "warning": 0})
+    movement.populate(-55, {"attack": True, "warning": 0})
+    movement.instances()
     pyibl.similarity(lambda x, y: 1-abs(x-y), "warning")
-    pyibl.similarity(lambda x, y: 1, "attack")
+    pyibl.similarity(lambda x, y: 1, "movement")
     return
 
  
 # this is the creation of the attacker agent, it will first choose a box, then whether or not to attack
 # the box given some feedback or "signal"
 def run(rounds=ROUNDS, participants=PARTICIPANTS):
-    agent_select = pyibl.Agent("SELECTION AGENT", default_utility=DEFAULT_UTILITY, optimized_learning=False)
-    agent_attack = pyibl.Agent("ATTACK AGENT", ["attack", "warning"], default_utility=DEFAULT_UTILITY, optimized_learning=False)
-    populate_agent(agent_select, agent_attack)
+    agent_movement = pyibl.Agent("ATTACK AGENT", ["movement", "warning"], default_utility=DEFAULT_UTILITY, optimized_learning=False)
+    populate_agent(agent_select, agent_movement)
     info = {"attack" : 0, "covered" : 0, "uncovered": 0, "withdraw": 0}
     for p in tqdm(range(participants)):             # for every participant they will partake in a given number of rounds
         reset_agent(agent_select)
-        reset_agent(agent_attack)
+        reset_agent(agent_movement)
         COVERAGE = np.random.choice([0,1], size=(ROUNDS,), p=[0.25, 0.75])  # this is where the odds for the defender can be altered for picking boxes
         for r in range(rounds):
             selection = agent_select.choose(0, 1)
             warned = 1 if selection == COVERAGE[r] else 0 if random.random() > 0.25 else 1
-            attack = agent_attack.choose({"attack": False, "warning": warned}, {"attack": True, "warning": warned})["attack"]
+            movement = agent_movement.choose({"attack": False, "warning": warned}, {"attack": True, "warning": warned})["attack"]
             covered = selection == COVERAGE[r]
-            if attack:
+            if movement:
                 info["attack"] += 1
                 payoff = -50 if covered else 100
                 info["covered"] += 1 if payoff == -50 else 0
@@ -66,7 +62,7 @@ def run(rounds=ROUNDS, participants=PARTICIPANTS):
                 info["withdraw"] += 1
                 payoff = 0
             
-            agent_attack.respond(payoff)
+            agent_movement.respond(payoff)
             agent_select.respond(payoff)
     print(info)
     return [info["attack"] / (ROUNDS * PARTICIPANTS), info["covered"] / (ROUNDS * PARTICIPANTS), info["uncovered"] / (ROUNDS * PARTICIPANTS), info["withdraw"] / (ROUNDS * PARTICIPANTS)]
